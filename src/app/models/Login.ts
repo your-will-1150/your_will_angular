e969c0ca0c1263c92c6de525e3a5fa5d87cbb5b2
jdrakecode <jdrakepaypal@gmail.com>
2019-07-24 17:01:20 -0400

    user login WIP

--- src/app/models/Login.ts
+++
@@ -1,6 +0,0 @@
-export interface Login {
-    username: string;
-    email: string;
-    password: string;
-    confirm_password: string;
-}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { RegisterUser } from '../models/RegisterUser';
 import { HttpClient } from '@angular/common/http';
+import { LoginUser } from '../models/LoginUser';
+import { Token } from '../models/Token';
 
 const Api_Url = "http://localhost:5000";
 
@@ -14,4 +16,14 @@ export class AuthService {
   register(regUserData: RegisterUser) {
     return this._http.post(`${Api_Url}/user/all`, regUserData);
   }
+
+  login(loginUserData: LoginUser) {
+    return this._http.post(`${Api_Url}/auth/login`, loginUserData).subscribe
+    ( (token: Token) => {
+      localStorage.setItem('auth_token', token.Authorization);
+      this._router.navigate(['/']);
+      this.isLoggedIn.next(true);
+    });
+  }
+
 }
