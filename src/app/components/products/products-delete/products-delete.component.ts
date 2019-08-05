3aa2f489cd535751a8ab1e16be23a324cdb30446
jdrakecode <jdrakepaypal@gmail.com>
2019-08-05 15:49:16 -0400

    item update working

--- src/app/components/products/products-delete/products-delete.component.ts
+++ src/app/components/products/products-delete/products-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Item } from 'src/app/models/Item';
 
 @Component({
   selector: 'app-products-delete',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsDeleteComponent implements OnInit {
 
-  constructor() { }
+  item: Item;
+  
+  constructor(private _productService: ProductService, private _activatedRoute: ActivatedRoute, private _router: Router) {
+    this._activatedRoute.paramMap.subscribe(p => {
+      this._productService.readItemDetail(p.get('id')).subscribe((singleItem: Item) => {
+        this.item = singleItem;
+      });
+    });
+  }
 
   ngOnInit() {
   }
 
+  onDelete() {
+    this._productService.deleteItem(this.item.id)
+  }
 }
